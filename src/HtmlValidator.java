5caded2f977132d2b2b0a4c7d50152dce7e8c6a9
davhx03 <daherreran@unal.edu.co>
2024-05-28 22:23:59 -0500

    Changes made

--- src/HtmlValidator.java
+++ src/HtmlValidator.java
@@ -4,7 +4,7 @@ import java.util.Stack;
 public class HtmlValidator {
 
 	public static Stack<HtmlTag> isValidHtml(Queue<HtmlTag> tags) {
-
+		Stack<HtmlTag> stackTags = new Stack<>();
 
 		// Verificar si la cola que entra que entra esta vacia
 		//vacio, return retorna vacia,
@@ -12,29 +12,40 @@ public class HtmlValidator {
 		//1. verificamos que no sea una etiqueta de autocierre, y continuamos
 		// isemty, mira si está vacía, isopentag las abiertas, de autocierre isselfclosing
 		//matches que compara si es cerrado y abierto
+		//Test 1: <u><b></b><i></i></u>
+		//Test 2: <html><b> bold text <i> italic text </i>
+		//Test 3: <b> <i> bold and italic </b> </i>
+		//Test 4: <b> some text <i> </i> </b> </b> </html>
 
-
-		Stack<HtmlTag> stackTags = new Stack<>();
-
-		//mientras la cola no este este vacia hacer
-		for (HtmlTag currentTag : tags) {
-
-			//2. crear la pila con las abiertas
-			if (currentTag.isOpenTag()) {
-				// Push open tags to the stack
-				stackTags.push(currentTag);
-				tags.poll();
-			}//si pilas vacias es porque el html esta bien
+		if (tags.isEmpty()){
+			return null;
 		}
+			//mientras la cola no este este vacia hacer
+			for (HtmlTag currentTag : tags) {
+				//2. crear la pila con las abiertas
+				if (currentTag.isOpenTag()) {
+					// Push open tags to the stack
+					stackTags.push(currentTag);
+					//si pilas vacias es porque el html esta bien
+				}else if(!currentTag.isSelfClosing()){
+					if(currentTag.matches(stackTags.peek())){
+						stackTags.pop();
+					}else{
+						return stackTags;
+					}
+				}
 
-
-		//currentTag.matches(stackTags.peek()))
-		if(tags.poll().matches(stackTags.peek())){
-			stackTags.pop();
+		}
 			return stackTags;
-		} else {
-			return stackTags;//eliminas
-		}
+
+
+	//currentTag.matches(stackTags.peek()))
+//				if (tags.poll().matches(stackTags.peek())) {
+//					stackTags.pop();
+//				} else {
+//					return stackTags;  //eliminas
+//				}
+//			}
 
 
 //		HtmlTag openTag = stack.peek();
@@ -45,10 +56,6 @@ public class HtmlValidator {
 //			return stack;
 //		}
 
-		if (stackTags.isEmpty()){
-			return null;
-		}
-
-		return null; // this line is here only so this code will compile if you don't modify it
+//		return null; // this line is here only so this code will compile if you don't modify it
 	}
 }
